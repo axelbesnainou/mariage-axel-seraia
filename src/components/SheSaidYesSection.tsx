5bd678dc976ad424b296a0d45f05c256e8cc2d04
Axel <axel.besnainou@gmail.com>
2026-04-15 17:09:11 +0200

    Update SheSaidYesSection.tsx

--- src/components/SheSaidYesSection.tsx
+++ src/components/SheSaidYesSection.tsx
@@ -30,7 +30,8 @@ const SheSaidYesSection = () => {
             Paris streets to hidden trails.<br /><br />
             After years of love, shared dreams, and joy,<br />
             we're thrilled to share that two lives become<br />
-            one — forever — together.<br /><br />
+            one — forever — together.<br />
+            <button type="button" class="btn btn-outline-success">Itinéraire</button><br /><br />
           </p>
           <h2 className="font-display text-4xl md:text-5xl mb-6 text-foreground">
             <span className="italic font-body text-3xl">L'</span> After Mairie & Henné
